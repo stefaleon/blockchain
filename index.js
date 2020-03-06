e5fbe2dca11a0fbbecabed562cc39db15aa949fa
unknown <stefaleon@gmail.com>
2020-03-06 14:44:44 +0200

    the mineBlock method

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Block = require('./block');
 
-const block = new Block('ts123', 'LHabc0123', 'HASH123', 'some data');
+// const block = new Block('ts123', 'LHabc0123', 'HASH123', 'some data');
 
-console.log(block.toString());
-console.log(Block.genesis().toString());
+// console.log(block.toString());
+// console.log(Block.genesis().toString());
+
+const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
+console.log(fooBlock.toString());
